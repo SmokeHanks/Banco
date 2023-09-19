f72c83bd0e8298808d772f3049aab28b7910d392
Lucas <luck6233@gmail.com>
2023-09-19 02:37:43 -0300

    andamento

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" class="css">
+    <title>Document</title>
+
+</head>
+
+<body>
+    <div class="conteiner">
+<form action="" method="post">
+
+<div class="input-find">
+<h3>Area Restrita</h3>
+<label for="">Usuario</label>
+<input type="text" name="Usuario" placeholder="Digite seu Usuario"><br><br>
+<label for="">Senha</label>
+<input type=" number" name="Senha" placeholder="Digite sua Senha"><br><br>
+</div>
+
+<button type="submit" name="btnLogin" value="Acessar">Acessar</button><br><br>
+<div class="links-uteis">
+    <a href=""><p>Esqueceu sua Senha?</p></a>
+    <a href=""><p>Ainda nao sou cadastrado!</p></a>
+
+</div>
+</form>
+</div>
+
+<ul class="colorlib-bubbles">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+
+</ul>
+</body>
+</html>
